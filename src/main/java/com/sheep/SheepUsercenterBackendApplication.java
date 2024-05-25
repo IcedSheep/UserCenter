25c30c9e3eec5b74a877467e15b0365348e47dd1
sheep <2398069758@qq.com>
2024-05-25 18:58:43 +0800

    项目初始化

---
+++ src/main/java/com/sheep/SheepUsercenterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sheep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheepUsercenterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheepUsercenterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sheep/SheepUsercenterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheepUsercenterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
